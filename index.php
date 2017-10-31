c0677e8fdf565b5b15af2d0138b9457555d12040
Ben Furfie <benfurfie@bens-mac-mini.home>
2017-10-31 18:29:03 +0000

    Add remaining includes

--- index.php
+++ index.php
@@ -7,5 +7,11 @@
     </head>
     <body>
         <?php include 'inc/banner.html'; ?>
+        <?php include 'inc/navigation.html'; ?>
+        <?php include 'inc/video.html'; ?>
+        <?php include 'inc/cards.html'; ?>
+        <?php include 'inc/pricing.html'; ?>
+        <?php include 'inc/faqs.html'; ?>
+        <?php include 'inc/calltoaction.html'; ?>
     </body>
 </html>
