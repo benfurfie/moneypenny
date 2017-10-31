ebb09fa7caf2df4b27d4fea063530f92d6be6e8b
Ben Furfie <benfurfie@bens-mac-mini.home>
2017-10-31 18:24:06 +0000

    Intitial index code

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Moneypenny</title>
+        <link rel="stylesheet" href="/style.css">
+    </head>
+    <body>
+        <?php include 'inc/banner.html'; ?>
+    </body>
+</html>
