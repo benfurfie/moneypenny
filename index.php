e353dc32a8ea558f42066dab74089a00b22556c2
Ben Furfie <benfurfie@bens-mac-mini.home>
2017-11-06 14:37:03 +0000

    Add meta tags for header

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link rel="icon" href="images/favicon.ico" />
         <title>Moneypenny</title>
         <link rel="stylesheet" href="/style.css">
     </head>
