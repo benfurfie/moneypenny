1d7a4c6e6e18e3d97f183488dce55b18e6bb6dda
Ben Furfie <benfurfie@bens-mac-mini.home>
2017-11-06 15:20:56 +0000

    Add favicon to head

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <link rel="icon" href="images/favicon.ico" />
+        <link rel="icon" href="images/favicon.png" />
         <title>Moneypenny</title>
         <link rel="stylesheet" href="/style.css">
         <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700|Open+Sans:300" rel="stylesheet">
